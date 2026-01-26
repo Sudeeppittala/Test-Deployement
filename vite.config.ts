8c35878fb5bad169971cb7f237733ae5b97c371d
Sudeep Pittala <45554994+Sudeeppittala@users.noreply.github.com>
2026-01-26 11:00:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Matches the repository name exactly as seen in your logs
-  base: '/Test-Deployement/', 
+  base: '/', 
 })
